000c2d6a965a4b6b7b7297ab8c8c7d287a7b4092
Muhammad Abdullah Abrar <abdullahabrar4843@gmail.com>
2024-12-11 08:00:38 +0500

    Chapter 2 CodeWithHarry --- 11 Dec 2024

---
+++ CodeWithHarry/Chapter 02/04_opreator.py
@@ -0,0 +1,26 @@
+# Arthimetic Opreator
+
+a = 43
+b = 24
+c = a + b
+print(c)
+
+# Assignment Opreator
+a = 4 - 2
+b = 6
+b += 3
+
+print(a)
+print(b)
+
+# Comparison Opreator
+print(a > b)
+print(a < b)
+print(5!=5)
+print(5==5)
+
+# Logical Opreator
+d = True and False
+print(d)
+
+print(not(False))
---
+++ CodeWithHarry/Chapter 02/05_type_casting.py
@@ -0,0 +1,20 @@
+# Type Casting
+
+a = 5
+print(type(a))
+
+b = 5.5
+print(type(b))
+
+c = "Harry"
+print(type(c))
+
+d = False
+print(type(d))
+
+e = None
+print(type(e))
+
+# Changing a to string
+f = str(a)
+print(type(f))
