9883dedb44c8c5004bce577822103992df05022c
Muhammad Abdullah Abrar <abdullahabrar4843@gmail.com>
2024-12-12 08:27:13 +0500

    Chapter 03 CodeWithHarry --- 12 Dec 2024

---
+++ CodeWithHarry/Chapter 03/01_intro_string.py
@@ -0,0 +1,20 @@
+# String
+
+a = "Abdullah"
+b = 'Harry'
+c = '''Harry and Abdullah'''
+
+# String is Imutalbe
+# Aik bar agar string ban gai to usa change nahi kar sakta
+
+namelen = len(a)
+print(namelen)
+
+character_1 = a[0]
+print(character_1)
+
+# Slicing
+
+# var = var[staring_index:ending_index:jump_index]
+nameshort = a[0:4]
+print(nameshort)
---
+++ CodeWithHarry/Chapter 03/03_string_fuction.py
@@ -0,0 +1,20 @@
+# String Fuctions
+
+print(dir("String"))
+
+name = "Harry"
+print(len(name))
+
+print(name.endswith('rry'))
+print(name.endswith('rrb'))
+
+print(name.startswith('Ha'))
+print(name.startswith('Hb'))
+
+print(name.capitalize())
+
+# Use Chatgpt and write
+'''
+Give me some most used Python String Function with examples.
+'''
+
