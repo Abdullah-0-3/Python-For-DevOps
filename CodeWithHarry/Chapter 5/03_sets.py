39aa48cb156666c77db2002d96a7fece5b49e453
Muhammad Abdullah Abrar <abdullahabrar4843@gmail.com>
2024-12-14 08:07:42 +0500

    Chapter 05 CodeWithHarry --- 14 Dec 2024

---
+++ CodeWithHarry/Chapter 5/03_sets.py
@@ -0,0 +1,7 @@
+# Sets in Python
+
+s = {1, 3, 5}
+
+# Empty Set
+empty_set = set()
+print(type(empty_set))
---
+++ CodeWithHarry/Chapter 5/05_intersection_union.py
@@ -0,0 +1,22 @@
+# Intersection and Union of Sets
+
+s1 = {1, 2, 3, 4, 5}
+s2 = {4, 5, 6, 7, 8}
+s3 = {1, 2, 3}
+
+# Intersection of s1 and s2
+print('Intersection of s1 and s2:', s1.intersection(s2))
+
+# Intersection of s1 and s3
+print('Intersection of s1 and s3:', s1.intersection(s3))
+
+# Union of s1 and s2
+print('Union of s1 and s2:', s1.union(s2))
+
+# Union of s1 and s3
+print('Union of s1 and s3:', s1.union(s3))
+
+'''
+Union: The union of two sets is the set of elements that are in either set.
+Intersection: The intersection of two sets is the set of elements that are in both sets.
+'''
